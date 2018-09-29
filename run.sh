d2a79717c7530295de8c1ed65699b4bcbec2a4c5
Cam <caiming@CamdeMacBook-Pro.local>
2018-09-29 16:45:24 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# 切换到当前目录
+cd /Users/caiming/Desktop/vnpy_fxdayu_data
+
+# conda 虚拟环境
+source activate vnpy
+
+export PYTHONPATH=$PWD
+
+# 创建biance数据索引
+echo create binance index
+python binance/binance.py create
+
+# 更新biance数据
+echo update binance data
+python binance/binance.py update publish
+
+# 创建jaqs一分钟数据索引
+echo create jaqs m1 index
+python jqdata/jqdata.py create
+
+# 更新jaqs一分钟数据
+echo update jaqs m1 data
+python jqdata/jqdata.py update publish
+
+# 创建oanda数据索引
+echo create oanda index
+python oanda/m1.py create
+
+# 更新oanda数据
+echo update oanda data
+python oanda/m1.py update publish
